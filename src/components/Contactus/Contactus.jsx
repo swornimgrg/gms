a9b0b2bbdd7753e1abb5e0ee6656300a99be3d21
DESKTOP-9EOVHR7\Swornim <swornimgrg94@gmail.com>
2022-06-30 21:10:00 +0545

    Added key

--- src/components/Contactus/Contactus.jsx
+++ src/components/Contactus/Contactus.jsx
@@ -6,15 +6,15 @@ function Contactus() {
   return (
     <Container>
       <h2>CONTACT US</h2>
-      <div>
-        {/* <iframe
+      {/* <div>
+        <iframe
           width="600"
           height="450"
           style="border:0"
           loading="lazy"
-          src="https://www.google.com/maps/embed/v1/view?key=AIzaSyBhrHgXuk7htL2zJabTJ_j9CuCY6aRV0Ko"
-        ></iframe> */}
-      </div>
+          src={`https://www.google.com/maps/embed/v1/view?key=${proc.env.REACT_APP_GOOGLE_API_KEY}`}
+        ></iframe>
+      </div> */}
     </Container>
   );
 }
