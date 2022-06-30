91b52049079f81a60ded3b08e7a7011404777cb4
DESKTOP-9EOVHR7\Swornim <swornimgrg94@gmail.com>
2022-06-30 20:37:40 +0545

    Resized images

--- src/components/SlideShow/SlideShow.jsx
+++ src/components/SlideShow/SlideShow.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Carousel, Container, Row, Col } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 import "./SlideShow.css";
 
 function SlideShow() {
