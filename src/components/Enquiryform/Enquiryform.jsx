08ab771f51cfaf4165f20de2b0e4710de9eb71e6
Swornim <swornimgrg94@gmail.com>
2022-08-25 22:57:40 +0545

    Connected mail to enquiry form

--- src/components/Enquiryform/Enquiryform.jsx
+++ src/components/Enquiryform/Enquiryform.jsx
@@ -1,31 +1,65 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Form, Button } from "react-bootstrap";
+import Alert from "react-bootstrap/Alert";
 import "./Enquiryform.css";
+import emailjs from "@emailjs/browser";
 
 function Enquiryform() {
+  const form = useRef();
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [showError, setShowError] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        `service_o5cshl8`,
+        `template_ka78pqe`,
+        form.current,
+        `0RR3-wuLdDBJof2Sw`
+      )
+      .then(
+        (result) => {
+          if (result.text == "OK") {
+            setShowSuccess(true);
+          }
+          setShowError(true);
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
     <div className="form-container">
-      <Form>
-        <Form.Group className="mb-3">
+      <Alert show={showSuccess} variant="success">
+        <Alert.Heading>Sucessfully submitted!</Alert.Heading>
+      </Alert>
+      <Alert show={showError} variant="success">
+        <Alert.Heading>Error submitting!</Alert.Heading>
+      </Alert>
+      <Form ref={form} onSubmit={sendEmail}>
+        <Form.Group className="mb-3" controlId="formName">
           <Form.Label bsPrefix="form-label">FULL NAME</Form.Label>
-          <Form.Control placeholder="Full Name" />
+          <Form.Control name="name" />
         </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3" controlId="formEmail">
           <Form.Label bsPrefix="form-label">CONTACT EMAIL ADDRESS</Form.Label>
-          <Form.Control type="email" placeholder="Email Address" />
+          <Form.Control type="email" name="email" />
         </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className="mb-3" controlId="formNumber">
           <Form.Label bsPrefix="form-label">
             CONTACT TELEPHONE NUMBER
           </Form.Label>
-          <Form.Control placeholder="Password" />
+          <Form.Control name="number" />
         </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className="mb-3" controlId="formMessage">
           <Form.Label bsPrefix="form-label">YOUR ENQUIRY</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control as="textarea" rows={3} name="message" />
         </Form.Group>
 
         <Button variant="primary" type="submit">
