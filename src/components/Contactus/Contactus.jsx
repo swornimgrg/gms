b4b53637fff37f2edc047c8de8656238f3a3cd23
Swornim <swornimgrg94@gmail.com>
2022-10-19 17:16:21 +0100

    Updated contact us

--- src/components/Contactus/Contactus.jsx
+++ src/components/Contactus/Contactus.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Enquiryform from "../Enquiryform/Enquiryform";
 import "./Contactus.css";
 
@@ -7,14 +7,11 @@ function Contactus() {
   return (
     <Container className="wrapper">
       <h2>CONTACT US</h2>
-      <Row>
-        <Col md>
-          <div className="form-wrapper">
-            <p className="form-title">Get in touch with us!</p>
-            <Enquiryform />
-          </div>
-        </Col>
-      </Row>
+
+      <div className="form-wrapper">
+        <p className="form-title">Get in touch with us!</p>
+        <Enquiryform />
+      </div>
     </Container>
   );
 }
