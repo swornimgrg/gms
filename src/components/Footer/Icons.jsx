108540936cb9d48cdcf85bd5d9f6e82eb8b5b535
swornimgrg <swornimgrg94@gmail.com>
2024-11-20 22:46:30 +0000

    Address Update

--- src/components/Footer/Icons.jsx
+++ src/components/Footer/Icons.jsx
@@ -8,7 +8,7 @@ function Icons() {
       <p className="address">
         Gurkha Management Services Limited <br />
         Company Registration Number: 13696404 <br />
-        167-169 Great Portland Street 5th Floor London, England W1W 5PF <br />
+        Unit A, 82 James Carter Road, Mildenhall, IP28 7DE <br />
       </p>
       <div className="contact">
         <i className="bi bi-telephone-fill">07434832739</i>
